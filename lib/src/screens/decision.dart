c7aca0ba596b6e1e3917b7efa2bff8f4bfb31c5e
Miller Adulu <milleradulu@gmail.com>
2019-10-06 00:08:21 +0300

    Redirect to pages on login based on credentials status

---
+++ lib/src/screens/decision.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:rx_command/rx_command.dart';
+
+import 'package:authflow/src/screens/home.dart';
+import 'package:authflow/src/screens/sign_in.dart';
+import 'package:authflow/src/state/managers/auth_manager.dart';
+import 'package:authflow/src/utils/service_locator.dart';
+
+class DecisionPage extends StatefulWidget {
+  @override
+  _DecisionPageState createState() => _DecisionPageState();
+}
+
+class _DecisionPageState extends State<DecisionPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    sl<AuthManager>().fetchSavedCredentials();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<CommandResult<bool>>(
+        stream: sl<AuthManager>().fetchSavedCredentials.results,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          final result = snapshot.data;
+
+          if (result?.data is bool) _redirectToPage(context, Home());
+
+          if (result?.data == null) _redirectToPage(context, SignIn());
+
+          return Scaffold(body: Center(child: Text('Initializing ...'),),);
+        });
+  }
+
+  void _redirectToPage(BuildContext context, Widget page) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      MaterialPageRoute newRoute =
+          MaterialPageRoute(builder: (BuildContext contenxt) => page);
+
+      Navigator.of(context)
+          .pushAndRemoveUntil(newRoute, ModalRoute.withName('/decision'));
+    });
+  }
+}
