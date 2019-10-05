d9a7997e63cdd4862f44456e15799ac9b9406acf
Miller Adulu <milleradulu@gmail.com>
2019-10-06 00:09:21 +0300

    Scaffold auth initiation pages

---
+++ lib/src/screens/sign_in.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
