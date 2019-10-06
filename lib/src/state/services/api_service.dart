dbbeb3f2c0b9636ae13690d4a287dd8683149470
Miller Adulu <milleradulu@gmail.com>
2019-10-06 11:21:42 +0300

    Mock API calls

--- lib/src/state/services/api_service.dart
+++ lib/src/state/services/api_service.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:authflow/src/utils/http/network.dart';
 
 abstract class APIService {
@@ -8,31 +10,51 @@ abstract class APIService {
 class APIServiceInstance implements APIService {
   NetworkUtil _networkUtil = new NetworkUtil();
 
+  var rng = new Random();
+
   String baseUrl = 'http://example.com/api';
 
   @override
   Future signInUser(Map credentials) async {
-    final String signInUrl = '$baseUrl/auth/signin';
+    // TODO: Uncomment the API call
+    // final String signInUrl = '$baseUrl/auth/signin';
 
-    dynamic response = await _networkUtil.postReq(signInUrl, body: {
-      'email': credentials['email'],
-      'password': credentials['password']
-    });
+    // dynamic response = await _networkUtil.postReq(signInUrl, body: {
+    //   'email': credentials['email'],
+    //   'password': credentials['password']
+    // });
 
-    if (response == null) return null;
-    return response;
+    // if (response == null) return null;
+    // return response;
+
+    // Delays the sign in for a bit to mock an API call
+    Future.delayed(Duration(seconds: 4));
+
+    // Mocks validation from the API.
+    // True: Successful sign in
+    // False: Failed sign in
+    return rng.nextBool();
   }
 
   @override
   Future signUpUser(Map credentials) async {
-    final String signUpUrl = '$baseUrl/auth/signup';
+    // TODO: Uncomment the API call
+    // final String signUpUrl = '$baseUrl/auth/signup';
 
-    dynamic response = await _networkUtil.postReq(signUpUrl, body: {
-      'email': credentials['email'],
-      'password': credentials['password']
-    });
+    // dynamic response = await _networkUtil.postReq(signUpUrl, body: {
+    //   'email': credentials['email'],
+    //   'password': credentials['password']
+    // });
 
-    if (response == null) return null;
-    return response;
+    // if (response == null) return null;
+    // return response;
+
+    // Delays the sign in for a bit to mock an API call
+    Future.delayed(Duration(seconds: 4));
+
+    // Mocks validation from the API.
+    // True: Successful sign in
+    // False: Failed sign in
+    return rng.nextBool();
   }
 }
